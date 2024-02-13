022d2b2e5ba69784f17c0e10384a992c1fbf26ac
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-02-13 11:54:29 +0100

    added jwt

--- paginaWeb/back/index.js
+++ paginaWeb/back/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 const jwt = require('jsonwebtoken')
+
+const SECRET = process.env.SECRET ?? 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcwNzgxODYzMiwiaWF0IjoxNzA3ODE4NjMyfQ.UNs4JsOz4joWR-VYjo8HEmS0zYyTCEmGWU4-6WAMIWA'
+
+
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
@@ -11,9 +15,32 @@ app.get('/', (req, res) => {
   res.status(200).send('hola :D')
 })
 
-app.get('/1', (req, res) => {
-    res.status(200).send('hola caracola')
-  })
+app.get('/token', (req, res) => {
+  const { user } = { user: 'JaviInUse' }
+  const token = jwt.sign({
+    user,
+    exp: Date.now() + 60 * 60 * 1000
+  }, SECRET)
+  res.send({ token })
+})
+
+app.get('/private', (req, res) => {
+
+  try {
+    const { authorization } = req.headers
+    const token = authorization.split(' ')[1]
+    const decoded = jwt.verify(token, SECRET)
+
+    if(Date.now() > decoded.exp){
+      throw new Error('Token expired')
+    }
+
+    res.send(decoded.user = ('JaviInUse') ? 'Welcome' : 'Unauthorized')
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+
 // la última a la que va a llegar
 app.use((req, res) => {
   res.status(404).send('<h1>404</h1>')
