09c1a5666fcfcacddbb117c4f2caf7f2f56e0d4a
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-10 11:02:48 +0100

    Refactor useUserManagement hook to include editUser functionality

--- paginaWeb/front/GymClass/src/hooks/useUserManagement.js
+++ paginaWeb/front/GymClass/src/hooks/useUserManagement.js
@@ -1,38 +1,62 @@
-import { useCallback, useState, useContext, useEffect } from 'react'
-import { allUsersService, deleteUserService } from '../Services/adminService'
+import { useCallback, useState, useContext, useRef } from 'react'
+import { allUsersService, deleteUserService, updateUserService } from '../Services/adminService'
 
 import { UserContext } from '../Contexts/UserContext'
-import { useNavigate } from 'react-router-dom'
+import { passwordValidation, roleValidation } from '../Validations'
+import { AdminUsersContext } from '../Contexts/AdminUsersContext'
 
 export const useUserManagement = () => {
-    const [admins, setAdmins] = useState([])
-    const [clients, setClients] = useState([])
-    const [instructors, setInstructors] = useState([])
+    const { admins, setAdmins, clients, setClients, instructors, setInstructors } = useContext(AdminUsersContext)
     const [loading, setLoading] = useState(true)
     const [view, setView] = useState(false)
     const { jwt } = useContext(UserContext)
+    const previousData = useRef({ admins, clients, instructors })
+    const firstRender = useRef(true)
 
-    const navigate = useNavigate()
-
+    //TODO: pedir x separado administradores,... y no todos juntos
+    //TODO: GESTIONAR ERRORES
     const getUsers = useCallback(async () => {
-        if (jwt === null) {
-            navigate('/login')
+        // if (jwt === null) {
+        //     navigate('/login')
 
+        // }
+        if (firstRender.current === true) { console.log('firstRender') }
+        if (JSON.stringify(previousData.current.admins) === JSON.stringify(admins) && previousData.current.clients === clients && previousData.current.instructors === instructors && !firstRender.current) {
+            
+            return
         }
-        else {
-            const users = await allUsersService({ jwt })
-            setAdmins(users["admins"])
-            setClients(users["clients"])
-            setInstructors(users["instructors"])
-            setLoading(false)
+        setLoading(true)
+        const users = await allUsersService({ jwt })
+        setAdmins(users["admins"])
+        setClients(users["clients"])
+        setInstructors(users["instructors"])
+        setLoading(false)
+        previousData.current = { admins, clients, instructors }
+
+        if (firstRender.current === true) firstRender.current = false
+
+    }, [admins,clients,instructors])
+
+
+    const editUser = async ({ email, password, role }) => {
+
+        const passwordValidationResult = passwordValidation({ password })
+        const roleValidationResult = roleValidation({ role: role.toLowerCase() })
+
+        let error = passwordValidationResult && roleValidationResult
+
+        if (!error) return false
+
+        try {
+
+            await updateUserService({ email, password, role, jwt })
+            getUsers()
+            return true
+
+        } catch (error) {
+            return false
         }
-    }, [jwt, navigate])
-
-
-    //preguntar a stephan si estaría bien xd
-    useEffect(() => {
-        getUsers()
-    }, [admins, clients, instructors, getUsers])
+    }
 
     const deleteUser = async ({ email, role }) => {
         try {
@@ -59,12 +83,11 @@ export const useUserManagement = () => {
     }
 
     return {
-        admins,
-        clients,
-        instructors,
         loading,
+        editUser,
         deleteUser,
         view,
-        setView
+        setView,
+        getUsers
     }
 }
