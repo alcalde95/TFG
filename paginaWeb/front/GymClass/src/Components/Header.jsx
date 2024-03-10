8b498af0be3e9c9dd5bcb9385b567861c912f15a
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-10 11:01:40 +0100

    Add navigation and conditional rendering to Header component

--- paginaWeb/front/GymClass/src/Components/Header.jsx
+++ paginaWeb/front/GymClass/src/Components/Header.jsx
@@ -1,23 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import useUser from "../hooks/useUser"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { UserContext } from "../Contexts/UserContext"
 
 export const Header = () => {
     const { isLogged, logout } = useUser()
+    const { email, role } = useContext(UserContext)
+    const navigate = useNavigate()
+    const location = useLocation()
 
-    const { email } = useContext(UserContext)
+    useEffect(() => { 
+        if(!isLogged && location.pathname !== '/') navigate('/login')
+
+    }, [role, isLogged])
 
     return (
-        <header className="bg-slate-300 border-4 border-teal-500 rounded-md m-2 p-1 px-2 py-2 pe-0 text-right">
+        <header className="bg-slate-300 border-4 border-teal-500 rounded-md m-2 p-1 px-2 py-2 pe-0 flex flex-col sm:flex-row justify-center sm:justify-between text-center gap-4">
+            <div>
+                <button onClick={() => navigate(-1)}
+                    className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
+                >Volver
+                </button>
+            </div>
             {
                 isLogged
                     ?
-                        <>
-                            <span className="mr-2">{email}</span>
-                            <button className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500" onClick={logout}>Logout</button>
-                        </>
-                    : <Link className="bg-teal-500  h-20 border-2 border-teal-500 text-white p-1 rounded-md m-1 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500" to="/login">Login</Link>
+                    <div >
+                        <span className="mr-2">{email}</span>
+                        <button className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
+                            onClick={logout}
+                        >Logout
+                        </button>
+                    </div>
+                    : <Link className="bg-teal-500  h-10 border-2 border-teal-500 text-white p-1 rounded-md m-1 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500" to="/login">Login</Link>
             }
 
         </header>
@@ -25,4 +40,6 @@ export const Header = () => {
 
     )
 }
-//grid grid-cols-2 bg-slate-300 border-4 border-teal-500 rounded-md m-2 p-1 text-right
+
+
+//borders sin color. shadcn, verde
