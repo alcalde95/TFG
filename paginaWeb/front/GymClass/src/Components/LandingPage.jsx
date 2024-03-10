31b0d3b5c85c1fa434ca70d33b143664ed32626b
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-10 11:01:48 +0100

    Refactor LandingPage component to fetch and display classes

--- paginaWeb/front/GymClass/src/Components/LandingPage.jsx
+++ paginaWeb/front/GymClass/src/Components/LandingPage.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom"
 import { Header } from "./Header"
+import { useClasses } from "../hooks/useClasses"
+import { Classes } from "./Classes/Classes"
+import { useContext, useEffect } from "react"
+import { ClassesContext } from "../Contexts/ClassesContext"
 
 export const LandingPage = () => {
+    const {classes} = useContext(ClassesContext)
+    const { getClasses, loading } = useClasses()
+
+    useEffect(() => {
+        getClasses()
+    }, [])
+
     return (
         <div className="max-w-6xl min-w-80 w-full min-h-screen flex flex-col">
             <Header />
@@ -11,24 +22,12 @@ export const LandingPage = () => {
                     <Link to="/login"><button className="bg-teal-500 w-40 border-4 border-teal-500 text-white p-2 rounded-md m-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500   ">Iniciar sesión</button></Link>
                     <Link to="/register"><button className="bg-teal-500 w-40 border-4 border-teal-500 text-white p-2 rounded-md m-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500   ">Registrarse</button></Link>
                 </nav>
-                <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2 w-auto">
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
-                    <div className="w-60 h-60 border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">placehodlder de las clases que se verán</div>
+                {
+                    loading
+                        ? <p className="text-2xl ">Cargando...</p>
+                        : <Classes classes={classes} editable={false} />
 
-                </section>
+                }
 
             </main>
         </div>
