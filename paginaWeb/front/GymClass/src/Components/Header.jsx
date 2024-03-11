926552dfe8fa95adcb3f9a03329d3cf8b13ca1db
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-11 20:52:24 +0100

    Fix login redirection bug in Header component

--- paginaWeb/front/GymClass/src/Components/Header.jsx
+++ paginaWeb/front/GymClass/src/Components/Header.jsx
@@ -12,6 +12,7 @@ export const Header = () => {
     useEffect(() => { 
         if(!isLogged && location.pathname !== '/') navigate('/login')
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [role, isLogged])
 
     return (
