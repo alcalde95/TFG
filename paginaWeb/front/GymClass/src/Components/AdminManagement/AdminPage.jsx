5141ad07e21dff7d067452dfd6e41926ccd8e0b3
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-11 20:52:54 +0100

    Update admin page navigation paths

--- paginaWeb/front/GymClass/src/Components/AdminManagement/AdminPage.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/AdminPage.jsx
@@ -8,11 +8,11 @@ export const AdminPage = () => {
             <Header />
             <main className="h-[80vh]  flex flex-col items-center text-center ">
                 <nav className="bg-slate-300 border-4 border-teal-500 rounded-md m-2 p-1 px-2 py-2 pe-0 text-right">
-                    <button onClick={() => navigate('/usersManagement')}
+                    <button onClick={() => navigate('/admin/usersManagement')}
                         className="bg-teal-500 m-1 w-[200px] h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500">
                         UserManagement
                     </button>
-                    <button onClick={() => navigate('/classesManagement')}
+                    <button onClick={() => navigate('/admin/classesManagement')}
                         className="bg-teal-500 m-1 w-[200px] h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500">
                         ClassesManagement
                     </button>
