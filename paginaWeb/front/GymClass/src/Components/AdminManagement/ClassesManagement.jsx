ce7ff248236d260269e5f976eb8f7e7d76493d5f
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-12 15:54:58 +0100

    Update class management title style

--- paginaWeb/front/GymClass/src/Components/AdminManagement/ClassesManagement.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/ClassesManagement.jsx
@@ -20,7 +20,7 @@ export const ClassesManagement = () => {
     <div className="max-w-6xl min-w-80 w-full min-h-screen flex flex-col ">
       <Header />
       <main className="h-full bg-slate-300 flex flex-col items-center  border-4 border-teal-500 rounded-md m-2 p-2 ">
-        <h1 className="text-4xl m-2">Gestión de Clases</h1>
+        <h1 className="text-4xl m-2 underline cursor-default">Gestión de Clases</h1>
         <div className="flex flex-col items-center text-center w-full">
 
           {
