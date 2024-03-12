2e25ff39dfa9320d64adaa2ea1a70c227c924c73
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-12 08:34:32 +0100

    Refactor Session component to display formatted date and add click event

--- paginaWeb/front/GymClass/src/Components/Session.jsx
+++ paginaWeb/front/GymClass/src/Components/Session.jsx
@@ -1,9 +1,21 @@
 
 export const Session = ({ session }) => {
-
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
     return (
-        <div key={session.data_time}>
+        <div className="bg-slate-400 text-center w-72 border-white border-2 rounded-lg h-20 
+                        flex flex-col items-center justify-center 
+                        hover:bg-slate-700 hover:text-white 
+                        transition duration-200 ease-in-out hover:cursor-pointer"
+            onClick={() => alert("🚧IN DEVELOPEMENT🚧") }
+        >
+            {/*TODO REVISAR ESTO*/}
             <p >{session.data_time}</p>
+            <p >{((new Date(session.data_time)).toLocaleString('es-ES',options))}</p>
         </div>
     )
 }
