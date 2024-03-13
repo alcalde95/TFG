73a0170946361b22eecaf2d1bebd6aa371bf4575
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-13 07:48:06 +0100

    Fix login redirection and add home button

--- paginaWeb/front/GymClass/src/Components/Header.jsx
+++ paginaWeb/front/GymClass/src/Components/Header.jsx
@@ -9,18 +9,24 @@ export const Header = () => {
     const navigate = useNavigate()
     const location = useLocation()
 
-    useEffect(() => { 
-        if(!isLogged && location.pathname !== '/') navigate('/login')
+    useEffect(() => {
+        if (!isLogged && location.pathname !== '/') navigate('/login')
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [role, isLogged])
 
     return (
         <header className="bg-slate-300 border-4 border-teal-500 rounded-md m-2 p-1 px-2 py-2 pe-0 flex flex-col sm:flex-row justify-center sm:justify-between text-center gap-4">
             <div>
+                <button onClick={() => navigate('/')}
+                    className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
+                >
+                    Inicio
+                </button>
                 <button onClick={() => navigate(-1)}
                     className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-                >Volver
+                >
+                    Volver
                 </button>
             </div>
             {
@@ -30,7 +36,8 @@ export const Header = () => {
                         <span className="mr-2">{email}</span>
                         <button className="bg-teal-500 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
                             onClick={logout}
-                        >Logout
+                        >
+                            Logout
                         </button>
                     </div>
                     : <Link className="bg-teal-500  h-10 border-2 border-teal-500 text-white p-1 rounded-md m-1 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500" to="/login">Login</Link>
