31792eaf20e196055cf32118302429d91670b49d
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-18 20:12:24 +0100

    Update ClassHeaderInfo component to use dynamic photo source

--- paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
+++ paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
@@ -2,7 +2,7 @@
 export const ClassHeaderInfo = ({ headerClass}) => {
     return (
         <div className="grid grid-cols-5 md:grid-cols-8 place-items-stretch justify-between w-full flex-wrap gap-2 border-2 rounded-md bg-slate-400 items-end text-center">
-            <img src="https://cataas.com/cat/says/hello" className="aspect-square  rounded-md rounded-r-none border-black md:row-span-2"></img>
+            <img src={headerClass.photo} className="aspect-square  rounded-md rounded-r-none border-black md:row-span-2"></img>
             <h1 className="col-span-2 p-2 font-bold text-3xl underline">{headerClass.name}</h1>
             <p className="col-span-2 p-2">Duración:{headerClass.duration}</p>
             <p className="col-span-2 p-2">Capacidad máxima:{headerClass.max_Capacity}</p>
