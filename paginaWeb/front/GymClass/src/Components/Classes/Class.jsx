e740ae7c73f28025915291dd34dea40d85f2a6b2
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-18 20:12:19 +0100

    Refactor Class component and update image source

--- paginaWeb/front/GymClass/src/Components/Classes/Class.jsx
+++ paginaWeb/front/GymClass/src/Components/Classes/Class.jsx
@@ -12,26 +12,26 @@ export const Class = ({ c, editable }) => {
     const handleClick = (e) => {
         e.preventDefault()
         if (!jwt) navigate("/login")
-        console.log()
 
         navigate(`${location.pathname}/${c.UUID_Class}`)
     }
-
     return (
         <>
             <div className="flex flex-col items-center border-2 w-10/12 border-teal-500 bg-gray-400 m-2 rounded-md   p-2 shadow-[2px_2px_5px_0px] shadow-gray-800 hover:cursor-pointer hover:bg-gray-700 hover:text-white transition-all duration-200 ease-in-out ">
                 <h1 className="font-bold text-3xl m-2 underline active:text-black hover:text-teal-500 transition-all duration-300 ease-in-out"
                     onClick={handleClick}>{c.name}</h1>
-                <img src="https://cataas.com/cat/says/hello" className="aspect-square w-11/12 lg:w-96 rounded-lg rounded-b-none border-2 border-b-0"></img>
-                <textarea defaultValue={c.description}
-                    data-tooltip-id={c.UUID_Class}
-                    className="w-11/12 lg:w-96  border-2  rounded-lg p-2 bg-slate-500 rounded-t-none border-t-0 text-slate-200 overflow-hidden "
-                />
+                    
+                <img src={c.photo.length > 50 ? c.photo : "https://picsum.photos/300/300" } alt={c.description} className="aspect-square w-11/12 lg:w-96 rounded-lg  border-2"></img>
+               
                 <section className="flex flex-col w-11/12 lg:w-96 justify-center items-center text-center m-4 p-4 bg-slate-500 border-2 rounded-lg text-slate-200">
+                    { jwt 
+                    ?
                     <p className="overflow-hidden w-11/12"
                         data-tooltip-id={c.UUID_Class + c.instructorEmail}>
                         {c.instructorEmail}
                     </p>
+                    : null
+                    }
                     <div className="flex flex-col md:flex-row w-full justify-around">
                         <p>Duración:{c.duration}&apos;</p>
                         <p>Aforo máximo:{c.max_Capacity}</p>
@@ -43,12 +43,6 @@ export const Class = ({ c, editable }) => {
                         : null
                 }
             </div>
-            <Tooltip
-                id={c.UUID_Class}
-                place="bottom"
-                className="h-auto max-w-xs"
-                content={c.description}
-            />
             <Tooltip
                 id={c.UUID_Class + c.instructorEmail}
                 place="bottom"
