d64fc4507ec2d1569ec3d63606b01eb2bf7ca4e7
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-19 20:06:25 +0100

    Add validation and error handling to createClass function

--- paginaWeb/front/GymClass/src/hooks/useClasses.js
+++ paginaWeb/front/GymClass/src/hooks/useClasses.js
@@ -1,12 +1,17 @@
 import { useCallback, useContext, useState } from "react"
 import { ClassesContext } from "../Contexts/ClassesContext"
-import { classesService } from "../Services/classService"
+import { classesInstructorService, classesService, createClassService } from "../Services/classService"
+import { classDescriptionValidation, classDurationValidation, classMaxCapacityValidation, classNameValidation } from "../Validations"
 
 export const useClasses = () => {
 
     const { setClasses } = useContext(ClassesContext)
     const [loading, setLoading] = useState(true)
-
+    const [nameError,setNameError] = useState("")
+    const [descriptionError,setDescriptionError] = useState("")
+    const [maxCapacityError,setMaxCapacityError] = useState("")
+    const [durationError,setDurationError] = useState("")
+    const [photoError,setPhotoError] = useState("")
     const getClasses = useCallback(async () => {
         try {
             setLoading(true)
@@ -16,11 +21,52 @@ export const useClasses = () => {
         } catch (error) {
             console.log(error)
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    const getInstructorClasses = async ({jwt}) => {
+        const res = await classesInstructorService({ jwt })
+        setClasses(res)
+    }
+
+
+    const createClass = async ({name, photo, description, maxCapacity, duration, instructorEmail}) =>{
+
+        const nameValidation = classNameValidation({name})
+        const descriptionValidation = classDescriptionValidation({description})
+        const maxCapacityValidation = classMaxCapacityValidation({maxCapacity})
+        const durationValidation = classDurationValidation({duration})
+        
+        setNameError(nameValidation)
+        setDescriptionError(descriptionValidation)
+        setMaxCapacityError(maxCapacityValidation)
+        setDurationError(durationValidation)
+        setPhotoError( photo ? "" : "Debe subir una foto")
+
+        if(nameValidation || descriptionValidation || maxCapacityValidation || durationValidation || !photo){
+            return false
+        }
+        try{
+            await createClassService({ name, photo, description, maxCapacity, duration, instructorEmail })
+            return true
+        }catch(error){
+            console.log(error)
+            return false
+        }
+        
+    }
+
 
     return {
         loading,
         getClasses,
+        getInstructorClasses,
+        createClass,
+        nameError,
+        descriptionError,
+        maxCapacityError,
+        durationError,
+        photoError,
+
     }
 }
