6e7d6a9ac8fada64637c533478c3a0b47cde6789
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-19 20:04:33 +0100

    added authorization on getSessions and added create and delete sessions funcrions

--- paginaWeb/back/rest/controllers/sessions.js
+++ paginaWeb/back/rest/controllers/sessions.js
@@ -1,39 +1,69 @@
-// import { validateSession } from '../schemas/class.js'
-
+import { partialValidateSession, validateSession } from '../schemas/session.js'
+import { authorized } from '../../utilFunctions.js'
 import { SessionsModel } from '../models/sessions.js'
-
+import { SECRET } from '../../index.js'
+import jwt from 'jsonwebtoken'
 export class SessionsController {
   static getSessions = async (req, res) => {
     try {
+      const { authorization } = req.headers
+      if (!authorization) return res.status(401).send('Unauthorized')
+      const token = authorization.split(' ')[1]
+      const userEmail = jwt.verify(token, SECRET).email
+      if (!authorized({ token })) return res.status(401).send('Unauthorized')
       const { classId } = req.params
-      const sessions = await SessionsModel.getSessions({ classId })
+      const sessions = await SessionsModel.getSessions({ classId, userEmail })
       res.json(sessions)
     } catch (error) {
-      res.status(500).send(error.message)
-    }
-  }
-/*
-  static getClass = async (req, res) => {
-    const { classId } = req.params
-    try {
-      const classData = await SessionsModel.getClass({ classId })
-      res.json(classData)
-    } catch (error) {
+      if (error.message === 'Unauthorized') return res.status(401).send(error.message)
       res.status(500).send(error.message)
     }
   }
 
-  static createClass = async (req, res) => {
-    const input = req.body
-    const validatedData = validateSession({ input })
-
-    if (validatedData.error) return res.status(400).send(validatedData.error.message)
-
+  static createSession = async (req, res) => {
     try {
-      await SessionsModel.create({ input })
+      const { authorization } = req.headers
+      if (!authorization) return res.status(401).send('Unauthorized')
+      const token = authorization.split(' ')[1]
+      const userEmail = jwt.verify(token, SECRET).email
+      if (!authorized({ token })) return res.status(401).send('Unauthorized')
+      const body = req.body
+      const input = {
+        dataTime: new Date(body.dataTime),
+        uuidClass: body.uuidClass,
+        instructorEmail: body.instructorEmail
+      }
+      const validatedData = validateSession({ input })
+
+      if (validatedData.error) return res.status(400).send(validatedData.error.message)
+
+      await SessionsModel.createSession({ input, userEmail })
       res.status(201).send('Created')
     } catch (e) {
       res.status(400).send('Bad request: ' + e.message)
     }
-  } */
+  }
+
+  static deleteSession = async (req, res) => {
+    try {
+      const { authorization } = req.headers
+      const token = authorization.split(' ')[1]
+      const userEmail = jwt.verify(token, SECRET).email
+      if (!authorized({ token })) res.status(401).send('Unauthorized')
+      const body = req.body
+
+      const input = {
+        dataTime: new Date(body.dataTime),
+        uuidClass: body.uuidClass
+      }
+
+      const validatedData = partialValidateSession({ input })
+
+      if (validatedData.error) return res.status(400).send(validatedData.error.message)
+      await SessionsModel.deleteSession({ input, userEmail })
+      res.send('Deleted')
+    } catch (error) {
+      res.status(500).send(error.message)
+    }
+  }
 }
