1b98cdf571460d1b95cffb4bce28b58a4005bbf2
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-19 20:04:48 +0100

    Add role-based authorization to getSessions, createSession, and deleteSession methods

--- paginaWeb/back/rest/models/sessions.js
+++ paginaWeb/back/rest/models/sessions.js
@@ -2,8 +2,19 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 export class SessionsModel {
-  static getSessions = async ({ classId }) => {
+  static getSessions = async ({ classId, userEmail }) => {
     try {
+      const role = await prisma.users.findUnique({
+        where: {
+          email: userEmail
+        },
+        select: {
+          role: true
+        }
+      })
+
+      if (role.role.toLowerCase() !== 'i') throw new Error('Unauthorized')
+
       const classes = await prisma.sessions.findMany({
         where: {
           UUID_Class: classId
@@ -15,38 +26,54 @@ export class SessionsModel {
     }
   }
 
-  /* static getClass = async ({ classId }) => {
-    try {
-      const classData = await prisma.class.findUnique({
-        where: {
-          UUID_Class: classId
-        }
-      })
-      return classData
-    } catch (e) {
-      throw new Error(e.message)
-    }
-  }
+  static createSession = async ({ input, userEmail }) => {
+    const role = await prisma.users.findUnique({
+      where: {
+        email: userEmail
+      },
+      select: {
+        role: true
+      }
+    })
 
-  static create = async ({ input }) => {
-    const { name, photo, description, maxCapacity, duration, instructorEmail } = input
-    const UUIDClass = uuidv4()
+    if (role.role.toLowerCase() !== 'i') throw new Error('Unauthorized')
+    const { dataTime, uuidClass, instructorEmail } = input
     try {
-      console.log(name, photo, description, maxCapacity, duration, instructorEmail, UUIDClass)
-
-      await prisma.class.create({
+      await prisma.sessions.create({
         data: {
-          UUID_Class: UUIDClass,
-          name,
-          photo,
-          description,
-          max_Capacity: maxCapacity,
-          duration,
+          data_time: dataTime,
+          UUID_Class: uuidClass,
           instructorEmail
         }
       })
     } catch (e) {
       throw new Error(e.message)
     }
-  } */
+  }
+
+  static deleteSession = async ({ input, userEmail }) => {
+    const role = await prisma.users.findUnique({
+      where: {
+        email: userEmail
+      },
+      select: {
+        role: true
+      }
+    })
+
+    if (role.role.toLowerCase() !== 'i') throw new Error('Unauthorized')
+    const { dataTime, uuidClass } = input
+    try {
+      await prisma.sessions.delete({
+        where: {
+          data_time_UUID_Class: {
+            data_time: dataTime,
+            UUID_Class: uuidClass
+          }
+        }
+      })
+    } catch (e) {
+      throw new Error(e.message)
+    }
+  }
 }
