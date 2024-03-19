55eb421d1bd36ab5fefff4fb118fec250d47bc50
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-19 20:05:13 +0100

    Refactor session schema and add partial validation

--- paginaWeb/back/rest/schemas/session.js
+++ paginaWeb/back/rest/schemas/session.js
@@ -1,13 +1,29 @@
 import z from 'zod'
 
 const sessionSchema = z.object({
-  data_time: z.date().refine((date) => date > new Date()),
+  dataTime: z.date().refine((date) => date > new Date(), { invalid_date: 'Date must be older than today' }),
+  uuidClass: z.string().uuid({
+    invalid_type_error: 'Invalid UUID format',
+    required_error: 'UUID is required'
+  }),
   instructorEmail: z.string().email({
     invalid_type_error: 'Invalid email format',
     required_error: 'Email is required'
   })
 })
 
+const partialSessionSchema = z.object({
+  dataTime: z.date().refine((date) => date > new Date(), { invalid_date: 'Date must be older than today' }),
+  uuidClass: z.string().uuid({
+    invalid_type_error: 'Invalid UUID format',
+    required_error: 'UUID is required'
+  })
+})
+
 export const validateSession = ({ input }) => {
   return sessionSchema.safeParse(input)
+}
+
+export const partialValidateSession = ({ input }) => {
+  return partialSessionSchema.safeParse(input)
 }
