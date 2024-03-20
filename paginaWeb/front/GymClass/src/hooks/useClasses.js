f86ded0aae2619929f107ce08192b5729cae72bc
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-20 20:01:44 +0100

    Refactor useClasses hook and add getClass function

--- paginaWeb/front/GymClass/src/hooks/useClasses.js
+++ paginaWeb/front/GymClass/src/hooks/useClasses.js
@@ -1,17 +1,17 @@
 import { useCallback, useContext, useState } from "react"
 import { ClassesContext } from "../Contexts/ClassesContext"
-import { classesInstructorService, classesService, createClassService } from "../Services/classService"
+import { classService, classesInstructorService, classesService, createClassService } from "../Services/classService"
 import { classDescriptionValidation, classDurationValidation, classMaxCapacityValidation, classNameValidation } from "../Validations"
 
 export const useClasses = () => {
 
     const { setClasses } = useContext(ClassesContext)
     const [loading, setLoading] = useState(true)
-    const [nameError,setNameError] = useState("")
-    const [descriptionError,setDescriptionError] = useState("")
-    const [maxCapacityError,setMaxCapacityError] = useState("")
-    const [durationError,setDurationError] = useState("")
-    const [photoError,setPhotoError] = useState("")
+    const [nameError, setNameError] = useState("")
+    const [descriptionError, setDescriptionError] = useState("")
+    const [maxCapacityError, setMaxCapacityError] = useState("")
+    const [durationError, setDurationError] = useState("")
+    const [photoError, setPhotoError] = useState("")
     const getClasses = useCallback(async () => {
         try {
             setLoading(true)
@@ -24,36 +24,46 @@ export const useClasses = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    const getInstructorClasses = async ({jwt}) => {
+    const getInstructorClasses = async ({ jwt }) => {
         const res = await classesInstructorService({ jwt })
         setClasses(res)
     }
 
+    const getClass = async ({ uuidClass, jwt }) => {
+        try {
+            const res = await classService({ uuidClass, jwt })
+            setClasses(res)
+        } catch (e) {
+            console.error(e.message)
+        }
+    }
 
-    const createClass = async ({name, photo, description, maxCapacity, duration, instructorEmail}) =>{
 
-        const nameValidation = classNameValidation({name})
-        const descriptionValidation = classDescriptionValidation({description})
-        const maxCapacityValidation = classMaxCapacityValidation({maxCapacity})
-        const durationValidation = classDurationValidation({duration})
-        
+
+    const createClass = async ({ name, photo, description, maxCapacity, duration, instructorEmail }) => {
+
+        const nameValidation = classNameValidation({ name })
+        const descriptionValidation = classDescriptionValidation({ description })
+        const maxCapacityValidation = classMaxCapacityValidation({ maxCapacity })
+        const durationValidation = classDurationValidation({ duration })
+
         setNameError(nameValidation)
         setDescriptionError(descriptionValidation)
         setMaxCapacityError(maxCapacityValidation)
         setDurationError(durationValidation)
-        setPhotoError( photo ? "" : "Debe subir una foto")
+        setPhotoError(photo ? "" : "Debe subir una foto")
 
-        if(nameValidation || descriptionValidation || maxCapacityValidation || durationValidation || !photo){
+        if (nameValidation || descriptionValidation || maxCapacityValidation || durationValidation || !photo) {
             return false
         }
-        try{
+        try {
             await createClassService({ name, photo, description, maxCapacity, duration, instructorEmail })
             return true
-        }catch(error){
+        } catch (error) {
             console.log(error)
             return false
         }
-        
+
     }
 
 
@@ -62,6 +72,7 @@ export const useClasses = () => {
         getClasses,
         getInstructorClasses,
         createClass,
+        getClass,
         nameError,
         descriptionError,
         maxCapacityError,
