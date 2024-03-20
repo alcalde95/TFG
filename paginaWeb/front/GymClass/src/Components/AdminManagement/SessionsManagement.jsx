b97261ad5e577f444568d8da995eadb55f68564e
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-20 20:01:53 +0100

    Refactor SessionsManagement component and add useSessions and useClasses hooks

--- paginaWeb/front/GymClass/src/Components/AdminManagement/SessionsManagement.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/SessionsManagement.jsx
@@ -1,42 +1,31 @@
 import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { createSessionService, sessionsService } from "../../Services/sessionsService"
 import { Header } from "../Header"
 import { Sessions } from "../Sessions"
-import { classService } from "../../Services/classService"
 import { ClassHeaderInfo } from "../Classes/ClassHeaderInfo"
 import { UserContext } from "../../Contexts/UserContext"
+import { useSessions } from "../../hooks/useSessions"
+import { SessionsContext } from "../../Contexts/SessionsContext"
+import { ClassesContext } from "../../Contexts/ClassesContext"
+import { useClasses } from "../../hooks/useClasses"
 
 export const SessionsManagement = () => {
-  const [sessions, setSessions] = useState([])
-  const [headerClass, setHeaderClass] = useState({})
+  
   const [show, setShow] = useState(false)
+  const { sessions } = useContext(SessionsContext)
+  const { classes } = useContext(ClassesContext)
   const { jwt } = useContext(UserContext)
+  
+  const { createSession, getSessions,dateError,instructorEmailError } = useSessions()
+  const { getClass } = useClasses()
+  
   const params = useParams()
 
-  const getSessionss = async ({ classId, jwt }) => {
-    try {
-      const res = await sessionsService({ classId, jwt })
-      setSessions(res)
-    } catch (e) {
-      console.error(e.message)
-    }
-  }
-  const getHeaderClass = async ({ classId, jwt }) => {
-    try {
-      const res = await classService({ classId, jwt })
-      setHeaderClass(res)
-    } catch (e) {
-      console.error(e.message)
-    }
-  }
-
-  //TODO: extrae el código de esta clase y componentizarla más
-
+  
   useEffect(() => {
-    const { classId } = params
-    getSessionss({ classId, jwt })
-    getHeaderClass({ classId, jwt })
+    const { uuidClass } = params
+    getSessions({ uuidClass, jwt })
+    getClass({ uuidClass, jwt })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -47,11 +36,10 @@ export const SessionsManagement = () => {
     const data = new FormData(form)
     const date = data.get("Date")
     const instructor = data.get("Instructor")
-    console.log(date, instructor)
-    try{
-      await createSessionService  ({ uuidClass: params.classId, dataTime:date, instructorEmail:instructor, jwt })
-    }catch(e){
-      console.error(e.message)
+    const res = await createSession({ uuidClass: params.uuidClass, dataTime: date, instructorEmail: instructor, jwt })
+    if (res) {
+      getSessions({ uuidClass: params.uuidClass, jwt })
+      setShow(false)
     }
   }
 
@@ -60,8 +48,8 @@ export const SessionsManagement = () => {
       <Header />
       <section className="h-full bg-slate-300 flex flex-col items-center border-4 border-teal-500 rounded-md m-2 p-2">
         {
-          headerClass && <ClassHeaderInfo headerClass={headerClass} />
-          
+          classes && <ClassHeaderInfo headerClass={classes} />
+
         }
       </section>
       <section className="h-full bg-slate-300 flex flex-col items-center border-4 border-teal-500 rounded-md m-2 p-2">
@@ -71,9 +59,15 @@ export const SessionsManagement = () => {
         {
           show
             ? <form className="flex flex-col items-center md:grid md:grid-cols-3 w-80 md:w-full border-2 border-teal-500 bg-gray-400 m-2 rounded-md gap-4 p-2 shadow-[2px_2px_5px_0px] shadow-gray-800"
-            onSubmit={handleSubmit}>
-              <p>Fecha clase:<input type="datetime-local" name="Date"/></p>
+              onSubmit={handleSubmit}>
+              <p>Fecha clase:<input type="datetime-local" name="Date" /></p>
+              {
+                dateError ? <p className="text-red-500">{dateError}</p> : null
+              }
               <input type="text" placeholder="Instructor a cargo" name="Instructor" />
+              {
+                instructorEmailError ? <p className="text-red-500">{instructorEmailError}</p> : null
+              }
               <button type="submit" className="bg-teal-500 w-40 h-10 border-2 border-teal-500 text-white rounded-md hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500">Añadir</button>
             </form>
             : null
