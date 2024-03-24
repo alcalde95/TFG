3f065a7ac8bcc131e45f35f01e53bec395e1db7b
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-24 21:13:32 +0100

    Refactor SessionsManagement component to add instructor selection

--- paginaWeb/front/GymClass/src/Components/AdminManagement/SessionsManagement.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/SessionsManagement.jsx
@@ -10,22 +10,23 @@ import { ClassesContext } from "../../Contexts/ClassesContext"
 import { useClasses } from "../../hooks/useClasses"
 
 export const SessionsManagement = () => {
-  
+
   const [show, setShow] = useState(false)
   const { sessions } = useContext(SessionsContext)
   const { classes } = useContext(ClassesContext)
-  const { jwt } = useContext(UserContext)
-  
-  const { createSession, getSessions,dateError,instructorEmailError } = useSessions()
+  const { jwt,email } = useContext(UserContext)
+
+  const { createSession, getSessions, getInstructors, dateError, instructorEmailError, instructors } = useSessions()
   const { getClass } = useClasses()
-  
+
   const params = useParams()
 
-  
+
   useEffect(() => {
     const { uuidClass } = params
     getSessions({ uuidClass, jwt })
     getClass({ uuidClass, jwt })
+    getInstructors({ jwt })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -54,9 +55,11 @@ export const SessionsManagement = () => {
       </section>
       <section className="h-full bg-slate-300 flex flex-col items-center border-4 border-teal-500 rounded-md m-2 p-2">
         <h1 className="text-4xl m-2">Sesiones</h1>
-        <button className="bg-teal-500 w-40 h-10 border-2 border-teal-500 text-white rounded-md hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-          onClick={() => setShow(!show)}>Añadir sesión</button>
         {
+        email === classes.instructorEmail && <button className="bg-teal-500 w-40 h-10 border-2 border-teal-500 text-white rounded-md hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
+          onClick={() => setShow(!show)}>Añadir sesión</button>
+        }
+          {
           show
             ? <form className="flex flex-col items-center md:grid md:grid-cols-3 w-80 md:w-full border-2 border-teal-500 bg-gray-400 m-2 rounded-md gap-4 p-2 shadow-[2px_2px_5px_0px] shadow-gray-800"
               onSubmit={handleSubmit}>
@@ -64,7 +67,15 @@ export const SessionsManagement = () => {
               {
                 dateError ? <p className="text-red-500">{dateError}</p> : null
               }
-              <input type="text" placeholder="Instructor a cargo" name="Instructor" />
+               <select name="Instructor"
+                  className="border-2 border-teal-500  md:w-full h-10 p-1 rounded-md m-0"
+                >
+                  {
+                    instructors.map((instructor, index) => {
+                      return <option key={index} value={instructor.email}>{instructor.email}</option>
+                    })
+                  }
+                </select>
               {
                 instructorEmailError ? <p className="text-red-500">{instructorEmailError}</p> : null
               }
