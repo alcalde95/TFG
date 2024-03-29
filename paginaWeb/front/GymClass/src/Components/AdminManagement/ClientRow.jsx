7b7ada20e30d5ef5511209695122354522b3ccf8
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-29 19:48:18 +0100

    Add validateClient function to ClientRow component

--- paginaWeb/front/GymClass/src/Components/AdminManagement/ClientRow.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/ClientRow.jsx
@@ -3,7 +3,7 @@ import { useUserManagement } from '../../hooks/useUserManagement'
 
 export const ClientRow = ({ client }) => {
 
-    const { deleteUser, editUser } = useUserManagement()
+    const { deleteUser, editUser,validateClient } = useUserManagement()
     const [showEdit, setShowEdit] = useState(false)
     const [userEditError, setUserEditError] = useState(false)
 
@@ -34,14 +34,22 @@ export const ClientRow = ({ client }) => {
         }
     }
 
+    const handleChange = () => {
+        console.log(client.client.validated === 'N')
+        const validated = client.client.validated === 'N' ? 'S' : 'N'
+        console.log(validated)
+        validateClient({ email: client.email, validated })
+    }
+
     return (
         <tr key={client.id} className='border-gray-500 border-[1px] '>
             <td>{client.email}</td>
-            <td>{client.role}</td>
-            <td><button className="bg-teal-500 m-1 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-                onClick={() => deleteUser({ email: client.email, role: client.role })}>
-                Eliminar
-            </button>
+            <td><input type='checkbox' checked={client.client.validated === 'N' ? false : true} onChange={handleChange}></input></td>
+            <td>
+                <button className="bg-teal-500 m-1 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
+                    onClick={() => deleteUser({ email: client.email, role: client.role })}>
+                    Eliminar
+                </button>
 
                 <button className="bg-teal-500 m-1 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
                     onClick={() => setShowEdit(!showEdit)}
@@ -52,7 +60,7 @@ export const ClientRow = ({ client }) => {
                     showEdit
                         ? <form name="editUserForm"
                             className="flex flex-col items-center w-10/12  md:w-auto lg:w-full m-2 gap-4 p-2 text-black"
-                            onSubmit={(e) => handleSubmit({ e, email: client.email, defaultPassword: client.password, defaultRole: client.role })}
+                            onSubmit={(e) => handleSubmit({ e, email: client.email, defaultPassword: client.password, defaultRole: 'C' })}
                         >
                             <input name="password"
                                 className="border-2 border-teal-500 w-10/12  md:w-full h-10 p-1 rounded-md  m-0"
