c9d139064c05ac5b00b829b78db581acba61f261
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-31 18:37:22 +0200

    Add updateSessionsClientsService function to sessionsClientsService.js

--- paginaWeb/front/GymClass/src/Services/sessionsClientsService.js
+++ paginaWeb/front/GymClass/src/Services/sessionsClientsService.js
@@ -13,3 +13,18 @@ export const sessionsClientsService = async ({jwt,UUIDClass,date}) =>{
     if (!res.ok) throw new Error('Response is NOT ok')
     return res.json()
 }
+export const updateSessionsClientsService = async ({jwt,uuidClass,dataTime,clientEmail,attend,justified}) =>{
+    const res = await fetch(`${ENDPOINT}/sessionsClients/${uuidClass}/${dataTime}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Authorization': `token ${jwt}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({dataTime,uuidClass,clientEmail,attend,justified})
+
+        }
+    )
+    if (!res.ok) throw new Error('Response is NOT ok')
+    return 
+}
