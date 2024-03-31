7c5229ae5177ea68af49ac8730b791fe34455da7
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-03-31 18:37:04 +0200

    Add updateSessionClients route to sessionsClientsRouter

--- paginaWeb/back/rest/routes/sessions_clients.js
+++ paginaWeb/back/rest/routes/sessions_clients.js
@@ -4,8 +4,9 @@ import { SessionsClientsController } from '../controllers/sessions_clients.js'
 export const sessionsClientsRouter = Router()
 
 sessionsClientsRouter.get('/:UUIDClass/:date', SessionsClientsController.getSessionClients)
-// classesRouter.get('/:', SessionsController.getSession)
 
 // sessionsRouter.post('/', SessionsController.createSession)
+
+sessionsClientsRouter.put('/:UUIDClass/:date', SessionsClientsController.updateSessionClients)
 
 // sessionsRouter.delete('/', SessionsController.deleteSession)
