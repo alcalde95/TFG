288c08194d65ffe1871f1a6e7ed74cf61ef98e93
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-02 19:56:00 +0200

    Add session information to SessionClientsManagement component

--- paginaWeb/front/GymClass/src/Components/SessionClientsManagement.jsx
+++ paginaWeb/front/GymClass/src/Components/SessionClientsManagement.jsx
@@ -8,14 +8,20 @@ import { UserContext } from "../Contexts/UserContext"
 import { ClassHeaderInfo } from "./Classes/ClassHeaderInfo"
 import { useSessionClients } from "../hooks/useSessionClients"
 import { SessionClients } from "./SessionClients/SessionClients"
+import { useSessions } from "../hooks/useSessions"
+import { SessionHeaderInfo } from "./SessionHeaderInfo"
+import { SessionContext } from "../Contexts/SessionContext"
 
 export const SessionClientsManagement = () => {
 
   const { sessionClients } = useContext(SessionClientsContext)
   const { classes } = useContext(ClassesContext)
+  const { session } = useContext(SessionContext)
   const { jwt } = useContext(UserContext)
 
   const { getClass } = useClasses()
+
+  const { getSession } = useSessions()
 
   const { getSessionClients } = useSessionClients()
 
@@ -25,16 +31,23 @@ export const SessionClientsManagement = () => {
     const { uuidClass, date } = params
     getClass({ uuidClass, jwt })
     getSessionClients({ jwt, UUIDClass: uuidClass, date })
+    getSession({ uuidClass, dataTime: date, jwt })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-
+  const x = sessionClients.length
+  console.log(x)
   return (
     <div className="max-w-6xl min-w-80 w-full min-h-screen flex flex-col ">
       <Header />
       <section className="h-full bg-slate-300 flex flex-col items-center border-4 border-teal-500 rounded-md m-2 p-2">
         {
-          classes && <ClassHeaderInfo headerClass={classes} />
+          classes && sessionClients && <ClassHeaderInfo headerClass={classes} num={x} />
+
+        }
+      </section >
+      <section className="h-full bg-slate-300 flex flex-col items-center border-4 border-teal-500 rounded-md m-2 p-2">
+        {
+          session && <SessionHeaderInfo session={session} num={x} />
 
         }
       </section >
