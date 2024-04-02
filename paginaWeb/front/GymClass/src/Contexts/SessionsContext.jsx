2c627409243597700d736ddcf5708adb3f76044b
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-02 19:56:39 +0200

    Add separate state variables for each day of the week in SessionsContext

--- paginaWeb/front/GymClass/src/Contexts/SessionsContext.jsx
+++ paginaWeb/front/GymClass/src/Contexts/SessionsContext.jsx
@@ -5,10 +5,32 @@ export const SessionsContext = createContext()
 
 export const SessionsContextProvider = ({ children }) => {
     const [sessions, setSessions] = useState([])
-
+    const [mondaySessions, setMondaySessions] = useState([])
+    const [tuesdaySessions, setTuesdaySessions] = useState([])
+    const [wednesdaySessions, setWednesdaySessions] = useState([])
+    const [thursdaySessions, setThursdaySessions] = useState([])
+    const [fridaySessions, setFridaySessions] = useState([])
+    const [saturdaySessions, setSaturdaySessions] = useState([])
+    const [sundaySessions, setSundaySessions] = useState([])
+    
     return <SessionsContext.Provider value={{
         sessions,
+        mondaySessions,
+        tuesdaySessions,
+        wednesdaySessions,
+        thursdaySessions,
+        fridaySessions,
+        saturdaySessions,
+        sundaySessions,
         setSessions,
+        setMondaySessions,
+        setTuesdaySessions,
+        setWednesdaySessions,
+        setThursdaySessions,
+        setFridaySessions,
+        setSaturdaySessions,
+        setSundaySessions
+
     }}>
         {children}
     </SessionsContext.Provider>
