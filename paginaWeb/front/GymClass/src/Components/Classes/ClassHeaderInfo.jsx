96fdf1f6ad14de3a3eccf86c198b76d3eb388328
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-04 08:05:41 +0200

    Update ClassHeaderInfo component styling

--- paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
+++ paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
@@ -1,9 +1,15 @@
 
-export const ClassHeaderInfo = ({ headerClass}) => {
+export const ClassHeaderInfo = ({ headerClass }) => {
     return (
-        <div className="grid grid-cols-5 md:grid-cols-8 place-items-stretch justify-between w-full flex-wrap gap-2 border-2 rounded-md bg-slate-400 items-end text-center">
+        <div className="sm:grid md:grid-cols-5 lg:grid-cols-8 
+        md:place-items-stretch md:justify-between md:items-end 
+        w-full 
+        flex flex-col flex-wrap 
+        gap-2 
+        border-2 rounded-md 
+        bg-slate-400  text-center">
             <img src={headerClass.photo} className="aspect-square  rounded-md rounded-r-none border-black md:row-span-2"></img>
-            <h1 className="col-span-2 p-2 font-bold text-3xl underline">{headerClass.name}</h1>
+            <h1 className="col-span-2 p-2 font-bold text-2xl underline">{headerClass.name}</h1>
             <p className="col-span-2 p-2">Duración:{headerClass.duration}</p>
             <p className="col-span-2 p-2">Capacidad máxima:{headerClass.max_Capacity}</p>
             <p className="col-span-2 p-2 text-xs md:text-base overflow-hidden ">{headerClass.instructorEmail}</p>
