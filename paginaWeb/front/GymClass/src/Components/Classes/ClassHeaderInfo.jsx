ac97f9e3a5d745910bea7ebb026a3e54521e9f87
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-04 20:50:34 +0200

    Refactor ClassHeaderInfo component styling

--- paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
+++ paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
@@ -1,19 +1,13 @@
 
 export const ClassHeaderInfo = ({ headerClass }) => {
     return (
-        <div className="sm:grid md:grid-cols-5 lg:grid-cols-8 
-        md:place-items-stretch md:justify-between md:items-end 
-        w-full 
-        flex flex-col flex-wrap 
-        gap-2 
-        border-2 rounded-md 
-        bg-slate-400  text-center">
-            <img src={headerClass.photo} className="aspect-square  rounded-md rounded-r-none border-black md:row-span-2"></img>
-            <h1 className="col-span-2 p-2 font-bold text-2xl underline">{headerClass.name}</h1>
+        <div className="sm:grid md:grid-cols-5 lg:grid-cols-8 md:place-items-stretch md:justify-between md:items-end w-full flex flex-col flex-wrap gap-2 border-2 rounded-md bg-slate-400 content-center justify-center items-center place-content-center text-center ">
+            <img src={headerClass.photo} className="aspect-square max-w-52 sm:max-w-[100px] rounded-md lg:rounded-r-none border-black md:row-span-2 m-2 lg:m-0"></img>
+            <h1 className="col-span-2 p-2 font-bold text-xl underline">{headerClass.name}</h1>
             <p className="col-span-2 p-2">Duración:{headerClass.duration}</p>
             <p className="col-span-2 p-2">Capacidad máxima:{headerClass.max_Capacity}</p>
             <p className="col-span-2 p-2 text-xs md:text-base overflow-hidden ">{headerClass.instructorEmail}</p>
-            <textarea defaultValue={headerClass.description} className="col-span-5 m-2 max-h-24 w-auto text-center  bg-gray-800 text-gray-200 appearance-non rounded-md" />
+            <textarea defaultValue={headerClass.description} className="box-content col-span-5 m-auto mb-2 lg:m-0 max-h-24 w-11/12 text-center  bg-gray-800 text-gray-200 appearance-non rounded-md"/>
         </div>
     )
 }
