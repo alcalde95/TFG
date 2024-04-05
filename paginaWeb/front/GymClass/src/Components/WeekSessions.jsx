eff2c5c2f3946ecc123816d47331a5137e35bcb8
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-05 19:35:01 +0200

    Update session summaries to handle null values in WeekSessions.jsx

--- paginaWeb/front/GymClass/src/Components/WeekSessions.jsx
+++ paginaWeb/front/GymClass/src/Components/WeekSessions.jsx
@@ -5,37 +5,37 @@ export const WeekSessions = ({ mondaySessions, tuesdaySessions, wednesdaySession
         <div className="w-full flex flex-row flex-wrap">
             <p className="bg-lime-700 w-full h-3 m-2 rounded-full" />
             {mondaySessions && <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Lunes({mondaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Lunes({mondaySessions?.length})</summary>
                 <Sessions sessions={mondaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>}
             <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Martes({tuesdaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Martes({tuesdaySessions?.length})</summary>
                 <Sessions sessions={tuesdaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>
             <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Miércoles({wednesdaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Miércoles({wednesdaySessions?.length})</summary>
                 <Sessions sessions={wednesdaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>
             <details className="open:w-full">
-                <summary className="text-2xl  m-2 underline">Jueves({thursdaySessions.length})</summary>
+                <summary className="text-2xl  m-2 underline">Jueves({thursdaySessions?.length})</summary>
                 <Sessions sessions={thursdaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>
             <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Viernes({fridaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Viernes({fridaySessions?.length})</summary>
                 <Sessions sessions={fridaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>
             <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Sábado({saturdaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Sábado({saturdaySessions?.length})</summary>
                 <Sessions sessions={saturdaySessions} />
                 <p className="bg-lime-700 open:w-full h-3 m-2 rounded-full" />
             </details>
             <details className="open:w-full">
-                <summary className="text-2xl m-2 underline">Domingo({sundaySessions.length})</summary>
+                <summary className="text-2xl m-2 underline">Domingo({sundaySessions?.length})</summary>
                 <Sessions sessions={sundaySessions} />
             </details>
             <p className="bg-lime-700 w-full h-3 m-2 rounded-full" />
