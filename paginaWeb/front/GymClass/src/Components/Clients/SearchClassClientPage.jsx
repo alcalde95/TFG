371790778c49ea61f0a1fd1e869860833b029a32
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-06 18:24:21 +0200

    Refactor SearchClassClientPage component to include loading state and useClasses hook

--- paginaWeb/front/GymClass/src/Components/Clients/SearchClassClientPage.jsx
+++ paginaWeb/front/GymClass/src/Components/Clients/SearchClassClientPage.jsx
@@ -1,10 +1,26 @@
+import { useContext, useEffect, useState } from 'react'
+import { ClassesContext } from '../../Contexts/ClassesContext'
 import { Header } from '../Header'
+import { Classes } from '../Classes/Classes'
+import { useClasses } from '../../hooks/useClasses'
 
 export const SearchClassClientPage = () => {
+
+    const [loading, setLoading] = useState(true)
+
+    const { getClasses } = useClasses()
+
+    const { classes, setClasses } = useContext(ClassesContext)
+
+    useEffect(() => {
+        setClasses([])
+        setLoading(false)
+    }, [])
 
     const handleSubmit = async (e) => {
 
         e.preventDefault()
+        setLoading(true)
         let form = e.target
         let data = new FormData(form)
         let name = data.get("name")
@@ -12,14 +28,14 @@ export const SearchClassClientPage = () => {
         let minDuration = data.get("minDuration")
         let maxDuration = data.get("maxDuration")
         console.log(name, maxCapacity, minDuration, maxDuration)
-
-
+        await getClasses({ name, maxCapacity, minDuration, maxDuration })
+        setLoading(false)
     }
 
     return (
-        <div className="max-w-6xl min-w-80 w-full h-screen flex flex-col">
+        <div className="max-w-6xl min-w-80 w-full min-h-screen h-full flex flex-col">
             <Header />
-            <main className="h-[90% ] bg-slate-300 flex flex-col items-center  border-4 border-teal-500 rounded-md m-2 p-2">
+            <section className=" bg-slate-300 flex flex-col items-center  border-4 border-teal-500 rounded-md m-2 p-2">
                 <form onSubmit={handleSubmit}>
                     <input type='text' name='name' placeholder='introduzca el nombre de la clase a buscar' />
                     <input type='number' name='maxCapacity' placeholder='nº max cli ' />
@@ -27,8 +43,17 @@ export const SearchClassClientPage = () => {
                     <input type='number' name='maxDuration' placeholder='duración máxima' />
                     <button>Buscar</button>
                 </form>
-            </main>
 
+            </section>
+            <main className=" bg-slate-300 flex flex-col items-center  border-4 border-teal-500 rounded-md m-2 p-2">
+                
+                {
+                    loading
+                        ? <p className="text-2xl ">Cargando...</p>
+                        : <Classes classes={classes} editable={false} managed={true} />
+
+                }
+            </main>
         </div>
     )
 }
