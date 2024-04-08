535422bd3df4865bcf4dbba4e22e241b69512af0
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-08 21:09:22 +0200

    Add ClientSessions component for displaying sessions for a specific client

---
+++ paginaWeb/front/GymClass/src/Components/Clients/ClientSessions.jsx
@@ -0,0 +1,29 @@
+import { ClientSessionCard } from "./ClientSessionCard"
+
+const ListOfSessions = ({ sessions }) => {
+    
+    return (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-4 p-2 w-full place-items-center ">
+            {
+                sessions.map((session) => (<ClientSessionCard key={session.data_time} session={session} />))
+            }
+        </ul>
+    )
+}
+
+const NoSessions = () => {
+    return (
+        <p className="text-center text-xl">No hay sesiones </p>
+    )
+}
+
+export const ClientSessions = ({ sessions}) => {
+    const hasSessions = sessions?.length > 0
+    return (
+
+        hasSessions
+            ? <ListOfSessions sessions={sessions} />
+            : <NoSessions  />
+
+    )
+}
