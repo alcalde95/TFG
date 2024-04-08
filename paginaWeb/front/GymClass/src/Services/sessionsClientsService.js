6a840cd9734bfe7ca72c64723530c2571c1b236a
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-08 21:09:41 +0200

    Refactor sessionsClientsService.js to improve code readability and add new functionality

--- paginaWeb/front/GymClass/src/Services/sessionsClientsService.js
+++ paginaWeb/front/GymClass/src/Services/sessionsClientsService.js
@@ -1,6 +1,6 @@
 import { ENDPOINT } from "./settings"
 
-export const sessionsClientsService = async ({jwt,UUIDClass,date}) =>{
+export const sessionsClientsService = async ({ jwt, UUIDClass, date }) => {
     const res = await fetch(`${ENDPOINT}/sessionsClients/${UUIDClass}/${date}`,
         {
             method: 'GET',
@@ -13,7 +13,23 @@ export const sessionsClientsService = async ({jwt,UUIDClass,date}) =>{
     if (!res.ok) throw new Error('Response is NOT ok')
     return res.json()
 }
-export const updateSessionsClientsService = async ({jwt,uuidClass,dataTime,clientEmail,attend,justified}) =>{
+
+export const enrollClientToSessionService = async ({ jwt, dataTime, uuidClass, clientEmail }) => {
+    const res = await fetch(`${ENDPOINT}/sessionsClients/${uuidClass}/${dataTime}`,
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': `token ${jwt}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ clientEmail })
+        }
+    )
+    if (!res.ok) throw new Error('Response is NOT ok')
+    return
+}
+
+export const updateSessionsClientsService = async ({ jwt, uuidClass, dataTime, clientEmail, attend, justified }) => {
     const res = await fetch(`${ENDPOINT}/sessionsClients/${uuidClass}/${dataTime}`,
         {
             method: 'PUT',
@@ -21,10 +37,25 @@ export const updateSessionsClientsService = async ({jwt,uuidClass,dataTime,clien
                 'Authorization': `token ${jwt}`,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({dataTime,uuidClass,clientEmail,attend,justified})
+            body: JSON.stringify({ dataTime, uuidClass, clientEmail, attend, justified })
 
         }
     )
     if (!res.ok) throw new Error('Response is NOT ok')
-    return 
+    return
+}
+export const isEnrolledService = async ({ jwt, dataTime, uuidClass, clientEmail }) => {
+    const res = await fetch(`${ENDPOINT}/sessionsClients/${uuidClass}/${dataTime}/isEnrolled`,
+        {
+            method: 'POST',
+            headers: {
+                'Authorization': `token ${jwt}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ clientEmail })
+        }
+    )
+    if (!res.ok) throw new Error('Response is NOT ok')
+    
+    return res.json()
 }
