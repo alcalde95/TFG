5a802c6bca08707bb2a9cdac6c8703b1d9d3117e
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-16 20:48:24 +0200

    Cambiar estilos y agregar restricciones en la página de inicio

--- paginaWeb/front/GymClass/src/Components/LandingPage.jsx
+++ paginaWeb/front/GymClass/src/Components/LandingPage.jsx
@@ -6,22 +6,30 @@ import { useContext, useEffect } from "react"
 import { ClassesContext } from "../Contexts/ClassesContext"
 
 export const LandingPage = () => {
-    const {classes} = useContext(ClassesContext)
+    const { classes } = useContext(ClassesContext)
     const { getClasses, loading } = useClasses()
 
     useEffect(() => {
         getClasses()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
-        <div className="max-w-6xl min-w-80 w-full min-h-screen flex flex-col">
+        <div className="w-full min-w-80 min-h-screen flex flex-col">
             <Header />
-            <main className="h-full bg-slate-300 flex  flex-col place-content-start items-center border-4 border-teal-500 rounded-md m-2 p-2 ">
-                <h1 className="text-center font-bold text-3xl m-10">GymClass, tu gestor de clases profesional</h1>
-                <nav className="flex flex-row flex-wrap place-content-center mb-5">
-                    <Link to="/login"><button className="bg-teal-500 w-40 border-4 border-teal-500 text-white p-2 rounded-md m-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500   ">Iniciar sesión</button></Link>
-                    <Link to="/register"><button className="bg-teal-500 w-40 border-4 border-teal-500 text-white p-2 rounded-md m-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500   ">Registrarse</button></Link>
+            <main className="h-full flex flex-col place-content-start items-center m-2 p-2">
+                <h1 className="text-center text-white font-bold text-3xl m-10">GymClass, tu gestor de clases profesional</h1>
+                <nav className="flex flex-row flex-wrap justify-center items-center w-full gap-2">
+                    <Link to="/login">
+                        <button className="bg-[#09090B] w-auto min-w-52 h-10 text-white  p-1 rounded-md  hover:border-green-500 hover:text-green-500 ease-in-out duration-200 border border-gray-500">
+                            Iniciar sesión
+                        </button>
+                    </Link>
+                    <Link to="/register">
+                        <button className="bg-[#09090B] w-auto min-w-52 h-10 text-white  p-1 rounded-md  hover:border-green-500 hover:text-green-500 ease-in-out duration-200 border border-gray-500">
+                            Registrarse
+                        </button>
+                    </Link>
                 </nav>
                 {
                     loading
