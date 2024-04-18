f3f19c3222854d36874c263de2b5cf01fb4f8363
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-18 20:07:06 +0200

    Cambiar estilos y agregar restricciones en el encabezado de la clase en ClassHeaderInfo.jsx

--- paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
+++ paginaWeb/front/GymClass/src/Components/Classes/ClassHeaderInfo.jsx
@@ -1,7 +1,7 @@
 
 export const ClassHeaderInfo = ({ headerClass }) => {
     return (
-        <div className="sm:grid md:grid-cols-5 lg:grid-cols-8 md:place-items-stretch md:justify-between md:items-end w-full flex flex-col flex-wrap gap-2 border-2 rounded-md bg-slate-400 content-center justify-center items-center place-content-center text-center ">
+        <div className="sm:grid md:grid-cols-5 lg:grid-cols-8 md:place-items-stretch md:justify-between md:items-end max-w-7xl w-full flex flex-col flex-wrap gap-2 border-2 rounded-md bg-slate-400 content-center justify-center items-center place-content-center text-center ">
             <img src={headerClass.photo} className="aspect-square max-w-52 sm:max-w-[100px] rounded-md lg:rounded-r-none border-black md:row-span-2 m-2 lg:m-0"></img>
             <h1 className="col-span-2 p-2 font-bold text-xl underline">{headerClass.name}</h1>
             <p className="col-span-2 p-2">Duración:{headerClass.duration}</p>
