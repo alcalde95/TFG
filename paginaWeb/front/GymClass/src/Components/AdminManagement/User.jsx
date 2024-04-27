cdfa0cbcb7601f6e91c94c5d2a48de05c50ced81
Jorge Alcalde <alcalde.ap95@gmail.com>
2024-04-27 12:45:57 +0200

    Corregir estilos y agregar restricciones en User.jsx

--- paginaWeb/front/GymClass/src/Components/AdminManagement/User.jsx
+++ paginaWeb/front/GymClass/src/Components/AdminManagement/User.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 import { useUserManagement } from "../../hooks/useUserManagement"
+import { DefaultButton, DefaultRedButton, FullWDefaultWhiteButton, InputMovinTitle } from "../CustomTailwindElements"
 
 
 export const User = ({ user }) => {
 
-    const { deleteUser,editUser } = useUserManagement()
+    const { deleteUser, editUser } = useUserManagement()
     const [showEdit, setShowEdit] = useState(false)
     const [userEditError, setUserEditError] = useState(false)
 
@@ -37,54 +38,41 @@ export const User = ({ user }) => {
 
 
     return (
-        <li key={user.email} className="w-full h-full border-green-700 border-2 rounded-md p-2 text-center bg-gray-400 text-white shadow-[2px_2px_5px_0px] shadow-gray-500">
+        <li key={user.email} className="w-full h-full border-gray-500 border rounded-md p-2 text-center bg-[#1C1917] text-white ">
             <p className="overflow-hidden">{user.email}</p>
-            <div className="flex flex-col  md:flex-row md:flex-wrap items-center justify-center ">
-                <button className="bg-teal-500 m-1 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-                    onClick={() => setShowEdit(!showEdit)}
-                >
-                    Editar
-                </button>
+            <div className="flex flex-col  md:flex-row md:flex-wrap items-center justify-center gap-2">
+                <DefaultButton handleClick={() => setShowEdit(!showEdit)} text="Editar" />
+
                 {
                     showEdit
                         ? <form name="editUserForm"
-                            className="flex flex-col items-center w-10/12  md:w-auto lg:w-full m-2 gap-4 p-2 text-black"
+                            className="flex flex-col items-center w-10/12  md:w-full  m-2 gap-4 p-2 text-white bg-[#09090B] border border-gray-500 rounded-lg"
                             onSubmit={(e) => handleSubmit({ e, email: user.email, defaultPassword: user.password, defaultRole: user.role })}
                         >
-                            <input name="password"
-                                className="border-2 border-teal-500 w-10/12  md:w-full h-10 p-1 rounded-md  m-0"
-                                type="text"
-                                placeholder="Contraseña"
-                                autoComplete="new-password"
-                            />
+                            <InputMovinTitle name='Contraseña' type={'password'} />
+
                             <select name="userType"
-                                className="border-2 border-teal-500 w-10/12 md:w-full h-10 p-1 rounded-md m-0"
+                                className="border border-green-500 w-full h-10 p-1 rounded-md m-0 text-center bg-transparent"
                             >
-                                <option value="A">
+                                <option value="A" className="text-center rounded-lg border border-green-500 bg-[#09090B]">
                                     Administrador
                                 </option>
-                                <option value="C">
+                                <option value="C" className="text-center rounded-lg border border-green-500 bg-[#09090B]">
                                     Cliente
                                 </option>
-                                <option value="I">
+                                <option value="I" className="text-center rounded-lg border border-green-500 bg-[#09090B]">
                                     Instructor
                                 </option>
                             </select>
                             {
                                 userEditError ? <p className="md:col-start-2 bg-red-600 text-white p-2 rounded-md m-2">Contraseña o rol incorrecto</p> : null
                             }
-                            <button className="w-full bg-teal-500 m-1 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-                                type="submit"
-                            >
-                                Guardar
-                            </button>
+                            <FullWDefaultWhiteButton text={"Guardar"} />
                         </form>
                         : null
                 }
-                <button className="bg-teal-500 m-1 w-20 h-10 border-2 border-teal-500 text-white p-1 rounded-md mr-2 hover:bg-teal-400 hover:border-white  shadow-[2px_2px_5px_0px] shadow-gray-500"
-                    onClick={() => deleteUser({ email: user.email, role: user.role })}>
-                    Eliminar
-                </button>
+                <DefaultRedButton handleClick={() => deleteUser({ email: user.email, role: user.role })} text="Eliminar" />
+
             </div>
         </li>
     )
